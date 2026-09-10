e4b60e80438c18e4a204567384af6b30906fed2d
DuongKhacBach <duongkhacbach@users.noreply.github.com>
2026-09-10 13:33:36 +0700

    Thong nhat route sinh-vien/trang-chu va tu dong mo form nhap lieu khi chay web

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -3,15 +3,22 @@ using DemoMVC.Models;
 
 namespace DemoMVC.Controllers;
 
+[Route("sinh-vien")]
 public class StudentController : Controller
 {
+    // GET: /sinh-vien/trang-chu
     [HttpGet]
+    [Route("trang-chu")]
+    [Route("")]
     public IActionResult Index()
     {
         return View();
     }
 
+    // POST: /sinh-vien/trang-chu
     [HttpPost]
+    [Route("trang-chu")]
+    [Route("")]
     public IActionResult Index(string fullName, string address, string school, string gender)
     {
         string result = $"Họ tên: {fullName} - Địa chỉ: {address} - Trường: {school} - Giới tính: {gender}";
